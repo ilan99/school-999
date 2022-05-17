c22a18cc2f6e7a0133c97431c954fff3897909d1
ilan99 <ilan99@hotmail.comgit config --global user.name ilan99>
2022-05-17 13:16:45 +0300

    improve app performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'school-app';
+
+  constructor(private studentService: StudentService) {}
+
+  // first server approach
+  ngOnInit(): void {
+    this.studentService.getAllStudents();
+  }
 }
